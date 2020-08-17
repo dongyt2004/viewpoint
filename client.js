dd736ae1546971fd6a080f3cff95c6475c186c69
dongyt2004 <dongyt2004@163.com>
2020-08-18 07:16:01 +0800

    1st - 2020.08.13

--- client.js
+++ client.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get("/", function (req, res) {
     res.status(200).render('viewpoint');
 });
-// 接收文本并生成摘要和脑图
+// 接收文本并生成主体、客体、观点
 app.post("/", function (req, res) {
     console.log('----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------');
     var text = '' + req.body;  // 原文
@@ -62,45 +62,39 @@ app.post("/", function (req, res) {
             res.header('Content-Type', 'text/plain; charset=utf-8').status(500).end(error.toString());
         } else {
             var ordered_spos = {};
-            Object.keys(spos).sort().forEach(function(key) {
+            var keys = Object.keys(spos);
+            keys.sort().forEach(function(key) {
                 ordered_spos[key] = spos[key];
             });
             console.log('spo=' + JSON.stringify(ordered_spos));  /////////////////
-            var viewpoint = '';
+            var subjects = new Array(keys.length);  // 主体
+            var viewpoints = new Array(keys.length);  // 观点
+            var objects = new Array(keys.length);  // 客体
             for(var line_no in ordered_spos) {
-                var viewpoint_start_index = 10000000;  // 观点的起始下标
-                for(i=0; i<ordered_spos[line_no][0].length; i++) {
+                var line_index = parseInt(line_no);
+                var viewpoint_start_index = 0;  // 观点的起始下标
+                for(var i=0; i<ordered_spos[line_no][0].length; i++) {
                     if ((typeof ordered_spos[line_no][0][i]['o']) !== 'string') {  // 如果宾语是[]，是一个宾语从句，一般代表观点
                         viewpoint_start_index = i;  // 找到观点的起始下标
                         break;
                     }
                 }
                 /*
-                主体  先不要去重
+                主体  先不去重
                 */
-                var subject = '';
-                for(var i=0; i<ordered_spos[line_no][0].length; i++) {
-                    if (ordered_spos[line_no][0][i]['s'] && ordered_spos[line_no][0][i]['s'] !== '' && i <= viewpoint_start_index) {
-                        subject = ordered_spos[line_no][0][i]['s'];
-                        break;
-                    }
-                }
-                /*
-                客体  先不要去重
-                */
-                var object = '';
-                for(i=0; i<ordered_spos[line_no][0].length; i++) {
-                    if (ordered_spos[line_no][0][i]['o'] && (typeof ordered_spos[line_no][0][i]['o']) === 'string' && ordered_spos[line_no][0][i]['o'] !== '' && i < viewpoint_start_index) {
-                        object = ordered_spos[line_no][0][i]['o'];
+                subjects[line_index] = '';
+                for(i=viewpoint_start_index; i>=0; i--) {
+                    if (ordered_spos[line_no][0][i]['s'] !== '') {
+                        subjects[line_index] = ordered_spos[line_no][0][i]['s'];
                         break;
                     }
                 }
                 /*
                 观点
                 */
-                var viewpoints = [];
+                viewpoints[line_index] = [];
                 var dedup_spos = dedup(ordered_spos[line_no][0], ordered_spos[line_no][1]);  // 去重
-                viewpoint_start_index = 0;  // 观点的起始下标
+                viewpoint_start_index = 1;  // 观点的起始下标，从1开始
                 for(i=0; i<dedup_spos[0].length; i++) {
                     if ((typeof dedup_spos[0][i]['o']) !== 'string') {  // 如果宾语是[]，是一个宾语从句，一般代表观点
                         viewpoint_start_index = i;  // 找到观点的起始下标
@@ -109,28 +103,105 @@ app.post("/", function (req, res) {
                 }
                 for(i=viewpoint_start_index; i<dedup_spos[0].length; i++) {
                     if ((typeof dedup_spos[0][i]['o']) === 'string') {
-                        viewpoints.push(dedup_spos[0][i]['s'] + dedup_spos[0][i]['p'] + dedup_spos[0][i]['o']);
+                        if (dedup_spos[0][i]['s'] !== '' && dedup_spos[0][i]['o'] !== '') {
+                            viewpoints[line_index].push(dedup_spos[0][i]['s'] + dedup_spos[0][i]['p'] + dedup_spos[0][i]['o']);
+                        }
                     } else {  // 宾语是宾语从句，代表观点
                         var obj_str = '';
-                        var object_clause = false;
                         for(var j=0; j<dedup_spos[0][i]['o'].length; j++) {
-                            if ((typeof dedup_spos[0][i]['o'][j]) !== 'string') {
-                                object_clause = true;
-                            }
-                            obj_str += stringify(dedup_spos[0][i]['o'][j]);
+                            obj_str += stringify_viewpoint(dedup_spos[0][i]['o'][j]) + "，";
                         }
-                        if (object_clause) {
-                            viewpoints.push(obj_str);
+                        if (obj_str.length > 0) {
+                            obj_str = obj_str.substr(0, obj_str.length - 1);
+                        }
+                        if (i === viewpoint_start_index) {  // 第一个观点不加谓语
+                            viewpoints[line_index].push(obj_str);
                         } else {
-                            viewpoints.push(dedup_spos[0][i]['p'] + obj_str);
+                            viewpoints[line_index].push(dedup_spos[0][i]['p'] + obj_str);
                         }
+
                     }
                 }
-                var no = parseInt(line_no) + 1;  // 段落号
-                viewpoint += '<span style="line-height: 10px;">' + no + '. 主体：' + subject + '<br>&nbsp;&nbsp;&nbsp;&nbsp;客体：' + object + '<br>&nbsp;&nbsp;&nbsp;&nbsp;观点：' + JSON.stringify(viewpoints) + '</span><br>';
             }
-            console.log('viewpoint=' + viewpoint);  /////////////////
-            res.status(200).json({'viewpoint': viewpoint});
+            console.log("主体=" + JSON.stringify(subjects));  //////////////////
+            console.log("观点=" + JSON.stringify(viewpoints));  //////////////////
+
+            var result = '';
+            for (var i=1; i<=subjects.length; i++) {
+                result += '<span style="line-height: 10px;">' + i + '. 主体：' + subjects[i-1] + '<br>&nbsp;&nbsp;&nbsp;&nbsp;观点：' + JSON.stringify(viewpoints[i-1]) + '</span><br>';
+            }
+            console.log('结果=' + result);  /////////////////
+            res.status(200).json({'viewpoint': result});
+
+            /*
+            客体
+            */
+            /*eachAsync(viewpoints, function(viewpoint_array, index, done) {
+                objects[index] = '';
+                if (viewpoint_array.length > 0) {
+                    request.post({
+                        url: "http://extract.ruoben.com:8008",   // http://extract-svc.nlp:44444
+                        headers: {
+                            "Content-Type": "text/plain"
+                        },
+                        body: viewpoint_array[0],
+                        timeout: 600000
+                    }, function (err, res, extract) {
+                        if (err) {
+                            done(err.toString());
+                        } else {
+                            if (res.statusCode === 200) {
+                                extract = JSON.parse(extract);
+                                for(var i = 0; i < extract.events.length; i++) {
+                                    if (extract.events[i].subject !== '' && extract.events[i].predicate !== '' && extract.events[i]['object'] !== '') {
+                                        objects[index] = extract.events[i].subject.replace(/{/g, "").replace(/}/g, "").replace(/\[/g, "").replace(/]/g, "").replace(/</g, "").replace(/>/g, "").replace(/《/g, "").replace(/》/g, "").replace(/`/g, "").replace(/\(/g, "").replace(/\)/g, "").replace(/~/g, "").replace(/«/g, "").replace(/»/g, "").replace(/【/g, "").replace(/】/g, "");
+                                        break;
+                                    }
+                                }
+                                // if (objects[index] === '') {
+                                //     for(i=0; i<viewpoint_start_index; i++) {
+                                //         if (ordered_spos[line_no][0][i]['o'] !== '') {
+                                //             object = ordered_spos[line_no][0][i]['o'];
+                                //             break;
+                                //         }
+                                //     }
+                                //     if (object === '') {
+                                //         for(i=0; i<=viewpoint_start_index; i++) {
+                                //             if (ordered_spos[line_no][1][i]['p'].indexOf('`') >= 0) {
+                                //                 object = ordered_spos[line_no][1][i]['p'].slice(ordered_spos[line_no][1][i]['p'].indexOf('`') + 1, ordered_spos[line_no][1][i]['p'].lastIndexOf('`'));
+                                //             } else if (ordered_spos[line_no][1][i]['p'].indexOf('[') >= 0) {
+                                //                 object = ordered_spos[line_no][1][i]['p'].slice(ordered_spos[line_no][1][i]['p'].indexOf('[') + 1, ordered_spos[line_no][1][i]['p'].indexOf(']'));
+                                //             } else if (ordered_spos[line_no][1][i]['p'].indexOf('《') >= 0) {
+                                //                 object = ordered_spos[line_no][1][i]['p'].slice(ordered_spos[line_no][1][i]['p'].indexOf('《') + 1, ordered_spos[line_no][1][i]['p'].indexOf('》'));
+                                //             } else {
+                                //                 object = ordered_spos[line_no][0][i]['p'];
+                                //             }
+                                //         }
+                                //     }
+                                // }
+                                done();
+                            } else {
+                                done("调用extract接口报错");
+                            }
+                        }
+                    });
+                } else {
+                    done();
+                }
+            }, function(error) {
+                if (error) {
+                    console.error(error);
+                    res.header('Content-Type', 'text/plain; charset=utf-8').status(500).end(error.toString());
+                } else {
+                    console.log("客体=" + JSON.stringify(objects));  //////////////////
+                    var result = '';
+                    for (var i=1; i<=subjects.length; i++) {
+                        result += '<span style="line-height: 10px;">' + i + '. 主体：' + subjects[i-1] + '<br>&nbsp;&nbsp;&nbsp;&nbsp;客体：' + objects[i-1] + '<br>&nbsp;&nbsp;&nbsp;&nbsp;观点：' + JSON.stringify(viewpoints[i-1]) + '</span><br>';
+                    }
+                    console.log('结果=' + result);  /////////////////
+                    res.status(200).json({'viewpoint': result});
+                }
+            });*/
         }
     });
 });
@@ -222,6 +293,26 @@ function stringify(spo_object) {
         } else {
             for(var index=0; index<spo_object.o.length; index++) {
                 s += stringify(spo_object.o[index]);
+            }
+        }
+    }
+    return s;
+}
+// 观点字符串化
+function stringify_viewpoint(spo_object, idx) {
+    var s = "";
+    if ((typeof spo_object) === 'string') {
+        s = spo_object;
+    } else {
+        s = spo_object.s + spo_object.p;
+        if ((typeof spo_object.o) === "string") {
+            s += spo_object.o;
+        } else {
+            for(var index=0; index<spo_object.o.length; index++) {
+                s += stringify_viewpoint(spo_object.o[index]) + "，";
+            }
+            if (s.length > 0) {
+                s = s.substr(0, s.length - 1);
             }
         }
     }
